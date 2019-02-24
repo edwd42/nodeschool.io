346914282e0e42dec3c2bcdba08d2a76ae99e86d
edwd42 <edwd42@yahoo.com>
2019-02-23 20:02:30 -0500

    completed exercise REVISING STRINGS (Exercise 5 of 19)

---
+++ javascripting/revising-strings.js
@@ -0,0 +1,28 @@
+var pizza = 'pizza is alright';
+pizza = pizza.replace('alright','wonderful');
+console.log(pizza);
+
+/**
+ * # JAVASCRIPTING
+
+## REVISING STRINGS (Exercise 5 of 19)
+
+ Here's the official solution in case you want to compare notes:
+
+─────────────────────────────────────────────────────────────────────────────
+
+# YES, PIZZA IS WONDERFUL.
+
+ Well done, with that .replace() method!
+
+ Next we will explore numbers.
+
+ Run javascripting in the console to choose the next challenge.
+
+─────────────────────────────────────────────────────────────────────────────
+ You've finished all the challenges! Hooray!
+
+─────────────────────────────────────────────────────────────────────────────
+ Need help? View the README for this workshop:
+ (http://github.com/sethvincent/javascripting)
+ */
