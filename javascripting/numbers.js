a2c84f63514c2b91d288d712d6f553ce4eb83e38
edwd42 <edwd42@yahoo.com>
2019-02-23 20:07:18 -0500

    completed exercise NUMBERS (Exercise 6 of 19)

---
+++ javascripting/numbers.js
@@ -0,0 +1,55 @@
+var example = 123456789;
+console.log(example);
+
+/**
+ * # JAVASCRIPTING  
+   
+ ## NUMBERS (Exercise 6 of 19)  
+   
+  Numbers can be integers, like 2, 14, or 4353, or they can be decimals,  
+  also known as floats, like 3.14, 1.5, or 100.7893423. Unlike Strings,  
+  Numbers do not need to have quotes.  
+   
+ ## The challenge:  
+   
+  Create a file named numbers.js.  
+   
+  In that file define a variable named example that references the integer  
+  123456789.  
+   
+  Use console.log() to print that number to the terminal.  
+   
+  Check to see if your program is correct by running this command:  
+   
+  javascripting verify numbers.js  
+   
+#################################
+
+  # JAVASCRIPTING
+
+## NUMBERS (Exercise 6 of 19)
+
+ Here's the official solution in case you want to compare notes:
+
+─────────────────────────────────────────────────────────────────────────────
+
+# YEAH! NUMBERS!
+
+ Cool, you successfully defined a variable as the number 123456789.
+
+ In the next challenge we will look at manipulating numbers.
+
+ Run javascripting in the console to choose the next challenge.
+
+─────────────────────────────────────────────────────────────────────────────
+ You've finished all the challenges! Hooray!
+
+─────────────────────────────────────────────────────────────────────────────
+ Need help? View the README for this workshop:
+ (http://github.com/sethvincent/javascripting)
+
+─────────────────────────────────────────────────────────────────────────────
+
+
+
+ */
