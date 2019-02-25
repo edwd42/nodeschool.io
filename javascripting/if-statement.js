c89cf2f76ba6e6fdd2b474b6fe806fb825e1dbe6
edwd42 <edwd42@yahoo.com>
2019-02-25 13:32:38 -0500

    completed exercise IF STATEMENT (Exercise 9 of 19)

---
+++ javascripting/if-statement.js
@@ -0,0 +1,80 @@
+var fruit = 'orange';
+if(fruit.length > 5){
+    console.log("The fruit name has more than five characters.")
+} else {
+    console.log("The fruit name has five characters or less.")
+}
+
+/**
+ * # JAVASCRIPTING  
+   
+ ## IF STATEMENT (Exercise 9 of 19)  
+   
+  Conditional statements are used to alter the control flow of a program,  
+  based on a specified boolean condition.  
+   
+  A conditional statement looks like this:  
+   
+     if (n > 1) {  
+       console.log('the variable n is greater than 1.');  
+     } else {  
+       console.log('the variable n is less than or equal to 1.');  
+     }  
+   
+  Inside parentheses you must enter a logic statement, meaning that the  
+  result of the statement is either true or false.  
+   
+  The else block is optional and contains the code that will be executed if  
+  the statement is false.  
+   
+ ## The challenge:  
+   
+  Create a file named if-statement.js.  
+   
+  In that file, declare a variable named fruit.  
+   
+  Make the fruit variable reference the value orange with the type of  
+  String.  
+   
+  Then use console.log() to print "The fruit name has more than five  
+  characters." if the length of the value of fruit is greater than five.  
+  Otherwise, print "The fruit name has five characters or less."  
+   
+  Check to see if your program is correct by running this command:  
+   
+     javascripting verify if-statement.js  
+   
+ ─────────────────────────────────────────────────────────────────────────────  
+  Need help? View the README for this workshop:  
+  (http://github.com/sethvincent/javascripting)  
+   
+ ─────────────────────────────────────────────────────────────────────────────  
+
+ #############################################
+
+ # JAVASCRIPTING
+
+## IF STATEMENT (Exercise 9 of 19)
+
+ Here's the official solution in case you want to compare notes:
+
+─────────────────────────────────────────────────────────────────────────────
+
+# CONDITIONAL MASTER
+
+ You got it! The string orange has more than five characters.
+
+ Get ready to take on for loops next!
+
+ Run javascripting in the console to choose the next challenge.
+
+─────────────────────────────────────────────────────────────────────────────
+ You've finished all the challenges! Hooray!
+
+─────────────────────────────────────────────────────────────────────────────
+ Need help? View the README for this workshop:
+ (http://github.com/sethvincent/javascripting)
+
+─────────────────────────────────────────────────────────────────────────────
+
+ */
