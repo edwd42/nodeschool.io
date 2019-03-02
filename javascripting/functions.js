d901f80b69f3fd1a4ac3b9fa7fee92c98b79b253
edwd42 <edwd42@yahoo.com>
2019-03-01 23:08:50 -0500

    refactored functions.js and add unit test

---
+++ javascripting/functions.js
@@ -0,0 +1,67 @@
+function eat(food){
+    return food + ' tasted really good.';
+}
+console.log(eat('bananas'));
+
+// refactor for jasmine unit testing
+module.exports = eat;
+
+/**
+ * ## FUNCTIONS (Exercise 17 of 19)  
+   
+  A function is a block of code that takes input, processes that input, and  
+  then produces output.  
+   
+  Here is an example:  
+   
+     function example (x) {  
+       return x * 2;  
+     }  
+   
+  We can call that function like this to get the number 10:  
+   
+     example(5)  
+   
+  The above example assumes that the example function will take a number as  
+  an argument –– as input –– and will return that number multiplied by 2.  
+   
+ ## The challenge:  
+   
+  Create a file named functions.js.  
+   
+  In that file, define a function named eat that takes an argument named  
+  food that is expected to be a string.  
+   
+  Inside the function return the food argument like this:  
+   
+     return food + ' tasted really good.';  
+   
+  Inside of the parentheses of console.log(), call the eat() function with  
+  the string bananas as the argument.  
+   
+  Check to see if your program is correct by running this command:  
+   
+     javascripting verify functions.js  
+   
+ ─────────────────────────────────────────────────────────────────────────────  
+ ######################################
+
+ ## FUNCTIONS (Exercise 17 of 19)
+
+ Here's the official solution in case you want to compare notes:
+
+─────────────────────────────────────────────────────────────────────────────
+
+# WOOO BANANAS
+
+ You did it! You created a function that takes input, processes that input,
+ and provides output.
+
+ Run javascripting in the console to choose the next challenge.
+
+─────────────────────────────────────────────────────────────────────────────
+ You've finished all the challenges! Hooray!
+
+─────────────────────────────────────────────────────────────────────────────
+
+ */
