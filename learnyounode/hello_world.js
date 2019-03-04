2c46e3bed6ac75ea337e698bfcce4d8f096992da
edwd42 <edwd42@yahoo.com>
2019-03-04 08:27:08 -0500

    completed exercise HELLO WORLD (Exercise 1 of 13)

---
+++ learnyounode/hello_world.js
@@ -0,0 +1,54 @@
+console.log("HELLO WORLD")
+
+/**
+ * # LEARN YOU THE NODE.JS FOR MUCH WIN!  
+   
+ ## HELLO WORLD (Exercise 1 of 13)  
+   
+  Write a program that prints the text "HELLO WORLD" to the console  
+  (stdout).  
+   
+ ─────────────────────────────────────────────────────────────────────────────  
+   
+ ## HINTS  
+   
+  To make a Node.js program, create a new file with a .js extension and  
+  start writing JavaScript! Execute your program by running it with the node  
+  command. e.g.:  
+   
+     $ node program.js  
+   
+  You can write to the console in the same way as in the browser:  
+   
+     console.log("text")  
+   
+  When you are done, you must run:  
+   
+     $ learnyounode verify program.js  
+   
+  to proceed. Your program will be tested, a report will be generated, and  
+  the lesson will be marked 'completed' if you are successful.  
+   
+ ─────────────────────────────────────────────────────────────────────────────
+
+## HELLO WORLD (Exercise 1 of 13)
+
+
+Your submission results compared to the expected:
+
+                 ACTUAL                                 EXPECTED                
+────────────────────────────────────────────────────────────────────────────────
+
+   "HELLO WORLD"                       ==    "HELLO WORLD"                      
+   ""                                  ==    ""                                 
+
+────────────────────────────────────────────────────────────────────────────────
+
+ ✓ 
+
+ Submission results match expected
+
+ # PASS Your solution to HELLO WORLD passed!
+
+ 
+ */
